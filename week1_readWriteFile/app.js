4881e6e7d871dec60351073cf2099b3e2d5f7eb1
David Bille <d_bille@yahoo.com>
2019-11-26 08:45:46 -0600

    still working

--- week1_readWriteFile/app.js
+++ week1_readWriteFile/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
   res.sendFile('./htmlFiles/home.html', { root:__dirname })
